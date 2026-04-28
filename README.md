d9a3007a84e2b123357dd09970deec08fe2af58b
upat <18456125+upat@users.noreply.github.com>
2026-04-29 01:33:47 +0900

    ファイル追加

---
+++ README.md
@@ -0,0 +1,52 @@
+# tiny_thermo
+概要
+---
+ATtiny85(Digispark)を使用し、乾電池駆動を目指した温度計
+
+機能
+---
+- 約1分毎に温度を計測し、LCDに表示
+- リセット確認用に駆動カウンタを表示(日/分)
+
+開発・動作環境
+---
+- 使用ボード
+    - Digispark
+- 画面表示モジュール
+    - 秋月電子通商 I2C接続小型LCDモジュール(8×2行)
+- 温湿度センサー
+    - DS18B20
+- Arduino IDE 2.3.6(使用ライブラリ)
+    - ATTinyCore 1.5.2 ([https://github.com/SpenceKonde/ATTinyCore](https://github.com/SpenceKonde/ATTinyCore))
+    - DS18B20 1.0.0 ([https://github.com/matmunk/DS18B20](https://github.com/matmunk/DS18B20))
+    - arduino_ST7032 ([https://github.com/tomozh/arduino_ST7032/](https://github.com/tomozh/arduino_ST7032/))
+
+使い方
+---
+- 配線例
+
+<img src="https://github.com/upat/tiny_thermo/blob/master/images/pic1.png" width="60%" height="60%">
+
+- 改造など
+    - 電池運用のため電源LED、PB1 LED、5V降圧レギュレーターを除去しています。
+    - PB1 LEDを除去せず使用する場合はPB1でDS18B20の通信が行えないため、PB3またはPB4にDS18B20を接続してください。
+    - 上記によりPB3またはPB4を使用する場合は、USB経由で書き込み時にDS18B20を外さないとスケッチが書き込めなくなります。
+
+ファイル構成
+---
+- LICENCE
+    - ライセンスファイル
+- README.md
+    - このファイル
+- tiny_thermo.ino
+    - メイン処理
+
+参考文献
+---
+- ATtiny85データシート ([https://ww1.microchip.com/downloads/en/devicedoc/atmel-2586-avr-8-bit-microcontroller-attiny25-attiny45-attiny85_datasheet.pdf](https://ww1.microchip.com/downloads/en/devicedoc/atmel-2586-avr-8-bit-microcontroller-attiny25-attiny45-attiny85_datasheet.pdf))
+- DS18B20データシート ([https://www.analog.com/media/en/technical-documentation/data-sheets/DS18B20.pdf](https://www.analog.com/media/en/technical-documentation/data-sheets/DS18B20.pdf))
+- AQM0802データシート(主にp.62) ([https://akizukidenshi.com/goodsaffix/st7032.pdf](https://akizukidenshi.com/goodsaffix/st7032.pdf))
+
+ライセンス
+---
+MIT Licence
---
+++ tiny_thermo.ino
@@ -0,0 +1,165 @@
+#include <ST7032.h>
+#include <DS18B20.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+
+DS18B20 ds(1);
+ST7032 lcd;
+
+volatile uint8_t wakeCounter = 0;
+
+void setup() {
+  /* ウォッチドッグタイマ割り込み設定 */
+  noInterrupts(); /* 割り込み禁止 */
+  /* WDTリセットフラグのクリア */
+  /* Bit 3 – WDRF: Watchdog Reset Flag */
+  MCUSR &= ~(1 << WDRF);
+  /* 4クロック間レジスタ設定変更許可、ウォッチドッグタイマ有効化 */
+  /* Bit 4 – WDCE: Watchdog Change Enable */
+  /* Bit 3 – WDE: Watchdog Enable */
+  WDTCR |= (1 << WDCE) | (1 << WDE);
+  /* ウォッチドッグタイムアウト割り込みフラグクリア、ウォッチドッグタイムアウト割り込み有効化、タイムアウト期間最大b1001=8s */
+  /* Bit 7 – WDIF: Watchdog Timeout Interrupt Flag */
+  /* Bit 6 – WDIE: Watchdog Timeout Interrupt Enable */
+  /* Bits 5, 2:0 – WDP[3:0]: Watchdog Timer Prescaler 3, 2, 1, and 0 */
+  WDTCR = (1 << WDIE) | (1 << WDP3) | (1 << WDP0) | (1 << WDIF);
+  interrupts();   /* 割り込み許可 */
+
+  /* PB2～PB5 A/D変換機能の無効化 */
+  /* Bit 7 – ADEN: ADC Enable */
+  ADCSRA &= ~(1 << ADEN);
+
+  /* 未使用ピン設定 */
+  pinMode(3, INPUT_PULLUP); /* デバッグ環境下ではここに接続したモジュールを外すこと */
+  pinMode(4, INPUT_PULLUP);
+
+  /* DS18B20 分解能設定(9bit ±0.5℃) */
+  ds.setResolution(9);
+
+  /* LCD設定 */
+  lcd.begin(8, 2);
+  lcd.setContrast(30); /* 環境によって変更(3.3V=30、5V=10) */
+}
+
+void loop() {
+  char dispStr[9] = {};
+  float tempVal = 0.0f;
+  static uint16_t minCounter = 0;
+  static uint16_t dayCounter = 0;
+
+  /* センサー値読み出し+表示 */
+  tempVal = ds.getTempC();
+  if (0.0 <= tempVal && 40.0 >= tempVal) {
+    lcd.setCursor(0, 0);
+    /* float→char変換(スペース埋め右詰め) */
+    convertFtoC(dispStr, tempVal);
+    /* 温度表示 */
+    lcd.print(dispStr);
+  } else {
+    lcd.setCursor(0, 0);
+    lcd.print("   Error");
+  }
+  
+  /* 0埋め3桁稼働日数/0埋め4桁稼働時間(分) */
+  lcd.setCursor(0, 1);
+  convertUtoC(dispStr, dayCounter, minCounter);
+  lcd.print(dispStr);
+
+  if (1440 > minCounter) {
+    minCounter++;   /* 実行回数カウンタ(分)加算 */
+  } else {
+    minCounter = 0;
+    if (999 > dayCounter) {
+      dayCounter++; /* 実行回数カウンタ(日)加算 */
+    } else {
+      dayCounter = 0;
+    }
+  }
+
+  wakeCounter = 0; /* カウンタクリア */
+  /* 約56s待ち */
+  while (wakeCounter < 7) {
+    goToSleep();
+  }
+}
+
+/* ウォッチドッグ割り込み */
+ISR(WDT_vect) {
+  /* カウンタ加算 */
+  wakeCounter++;
+}
+
+/*******************************************************************/
+/* 処理内容：スリープ実行関数                                      */
+/* 引数　　：なし                                                  */
+/* 戻り値　：なし                                                  */
+/* 備考　　：ATTiny85スリープ関数のコール                          */
+/*******************************************************************/
+void goToSleep(void) {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); /* スリープモード設定 */
+  sleep_enable();  /* スリープ有効化 */
+  interrupts();    /* 割り込み許可(割り込み禁止誤作動対策) */
+  sleep_cpu();     /* スリープ実行 */
+  sleep_disable(); /* スリープ無効化 */
+}
+
+/*******************************************************************/
+/* 処理内容：指定した形式のfloat->char変換                         */
+/* 引数　　：9Byteの配列ポインタ、0.0～40.0(室温想定)までの値      */
+/* 戻り値　：なし                                                  */
+/* 備考　　：ST7032 8x2行キャラクターLCD用                         */
+/*******************************************************************/
+void convertFtoC(char* strBuf, float tempVal) {
+  uint16_t intVal = 0;
+
+  /* 小数1桁に丸め(* 10 + 0.5(丸め)して整数化) */
+  intVal = (uint16_t)(tempVal * 10.0f + 0.5f);
+  /* 終端文字 */
+  strBuf[8] = '\0';
+  /* 温度記号 */
+  strBuf[7] = 'C';
+  strBuf[6] = 0xF2; /* ℃の°の部分 */
+  /* 小数部から順に格納 */
+  /* 小数部 */
+  strBuf[5] = '0' + intVal % 10;   /* asciiの0に足してchar変換 */
+  intVal /= 10;
+  /* 小数点 */
+  strBuf[4] = '.';
+  /* 整数部(1の位) */
+  strBuf[3] = '0' + intVal % 10;   /* asciiの0に足してchar変換 */
+  intVal /= 10;
+  /* 整数部(10の位) */
+  if (1 > intVal) {
+    strBuf[2] = ' ';
+  } else {
+    strBuf[2] = '0' + intVal % 10; /* asciiの0に足してchar変換 */
+  }
+  /* 空白(未使用箇所) */
+  strBuf[1] = strBuf[0] = ' ';
+}
+
+/*******************************************************************/
+/* 処理内容：指定した形式のuint16_t->char変換                      */
+/* 引数　　：9Byteの配列ポインタ、3桁の値、4桁の値                 */
+/* 戻り値　：なし                                                  */
+/* 備考　　：ST7032 8x2行キャラクターLCD用                         */
+/*******************************************************************/
+void convertUtoC(char* strBuf, uint16_t dayVal, uint16_t minVal) {
+  int i = 0;
+
+  /* 終端文字 */
+  strBuf[8] = '\0';
+  /* 0埋め4桁稼働時間(分) */
+  for (i = 7; i > 3; i--) {
+    strBuf[i] = '0' + (minVal % 10); /* asciiの0に足してchar変換(0の場合は0が入り続ける) */
+    minVal /= 10;
+  }
+  /* 区切り記号 */
+  strBuf[3] = '/';
+  /* 0埋め3桁稼働日数 */
+  for (i = 2; i >= 0; i--) {
+    strBuf[i] = '0' + (dayVal % 10); /* asciiの0に足してchar変換(0の場合は0が入り続ける) */
+    dayVal /= 10;
+  }
+}
